bbd3b890405dc592775b2bd19e38320118847782
alexmeng03 <alex.meng21@ycdsbk12.ca>
2020-11-27 13:59:22 +0000

    started #1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,19 @@
 package gr11review.part2;
 
 public class Utility {
+  // Create a method that for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+  public static String zipzap(String strInput){
+    String strSub;
+    String strReturn;
+    int intCount;
+    int intLength;
+
+    intLength = strInput.length();
+
+    for(intCount = 0; intCount < intLength; intCount++){
+      
+    }
+  }
     
 }
