a1b80d430e3fc43c05a26e9deecd4f5e920f5713
repl.it user <replituser@example.com>
2020-11-27 15:27:42 +0000

    Before Errors

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,13 @@
 package gr11review.part2;
 
+import java.io.*;
+
+public class Main{
+  public static void main (String[] args) throws IOException{
+
+    BufferedReader keyboard = new BufferedReader(new BufferedInputStreamReader(System.in));
+
+    
+
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,49 @@
 package gr11review.part2;
 
 public class Utility {
+  public static String R7(String strWord) {
     
+    String strAll = strWord;
+    String strReadAll;
+    int intCount = 0;
+    int intCountWords;
+    int intTotalChar = strAll.length();
+    int intCounter = 0;
+    int intNumbers;
+    int intCombined;
+    String strTempChar;
+    String strChar;
+    char Characters;
+
+    while (intCount != intTotalChar) {
+
+      Characters = strTheSentence.charAt(intCount);
+      intCount++;
+
+      while (Characters >= '0' && Characters <= '9') {
+        intCounter++;
+
+        strTempChar = Character.toString(Characters);
+
+        intCountWords = intCount + intCounter;
+        Characters = strTheSentence.charAt(intCountWords);
+
+        if (Characters >= '0' && Characters <= '9') {
+          strChar = Character.toString(Characters);
+          strTempChar = strTempChar + strChar;
+        }else if (Characters){
+
+          
+
+        }
+      }
+
+      intCount = intCount + intCountWords;
+
+      if (intCount >= intLength) {
+        intCount--;
+        break;
+      }
+    }
+  }
 }
