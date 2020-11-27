21a6c88a2a083116ca33909cf7772ea447518881
alexmeng03 <alex.meng21@ycdsbk12.ca>
2020-11-27 14:41:41 +0000

    trying to test

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,17 @@
 package gr11review.part2;
+import java.io.*;
 
+public class Main{
+  public static void main(String[] args) throws IOException{
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+    
+    String strInput;
+    String strOutput;
+
+    System.out.println("Type a word");
+    strInput = key.readLine();
+    strOutput = Utility.zipzap(strInput);
+    
+    System.out.println(strOutput);
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,13 +1,15 @@
 package gr11review.part2;
+import java.io.*;
 
-public class Utility {
+public class Utility{
   // Create a method that for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
 
   public static String zipzap(String strInput){
     String strSub;
-    String strReturn;
+    String strPart;
     String strLetter1;
     String strLetter3;
+    String strReturn;
     int intCount;
     int intNextCount;
     int intLength;
@@ -22,7 +24,8 @@ public class Utility {
       strLetter1 = strSub.substring(0 , 1);
       strLetter3 = strSub.substring(2 , 3);
       if(strLetter1.equalsIgnoreCase("z") && strLetter3.equalsIgnoreCase("p")){
-        strReturn = strLetter1 + strLetter3;
+        strPart = strLetter1 + strLetter3;
+        strReturn = strReturn + strPart;
       }
     }
 
