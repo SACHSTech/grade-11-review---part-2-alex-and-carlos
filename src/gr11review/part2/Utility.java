66663fc8a06c034ab1d93aeae76e78d99c8ed7ca
repl.it user <replituser@example.com>
2020-11-30 19:27:50 +0000

    die?

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,30 +1,32 @@
 package gr11review.part2;
 
 public class Utility {
-  public static int StringCombinedInt(String strWord) {
+  public static int sumNumbers(String strWord) {
 
     // here is what I'll do: 
     // create a loop that read entire sentence, when it finds a char that is a int, it registers into string variable to create a strings of integers. Then it starts to loop again but begins at the last time it read, if it finds a integer char variable, it'll start to loop and register the char variables behind.
   
     int intCount;
-    int intCount2;
+    int intCount2 = 0;
     int intTotal;
-    int intCombined;
+    int intCombined = 0;
     String strAll = strWord;
     String strSub;
-    int intNum;
-    int intNumCombined;
+    int intNum = 0;
+    int intNumCombined = 0;
     char Char1;
     char Char2;
 
     intTotal = strAll.length();
 
     for (intCount = 0; intCount <= intTotal; intCount++) {
-      intCount2 = 0;
+
+      intCount = intCount + intCount2;
 
       Char1 = strAll.charAt(intCount);
 
-      while (Charactor.isDigit(Char1)) {
+      while (Character.isDigit(Char1)) {
+
         intCount2++;
 
         Char2 = strAll.charAt(intCount + intCount2);
@@ -32,26 +34,32 @@ public class Utility {
         if (Character.isDigit(Char2)) {
 
           strSub = strAll.substring(intCount, intCount2);
-          break;
+          System.out.print(strSub);
+
+          intNum = Integer.parseInt(strSub);
+
         }else if (!Character.isDigit(Char2)) {
-          intCount2--;
 
           intCount = intCount + intCount2;
+          strSub = strAll.substring(intCount, intCount2);
           break;
+
         }
-        System.out.print(strSub);
-        
       }
 
+      intCombined = intCombined + intNum;
+
+      if (intCount > intTotal) {
+
+        intCount = intTotal;
+
+      }
 
     }
-
-  }
   return intCombined;
-}
-  public static int sumNumbers(String str) {
-    // Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+  }
+  public static String alphaWord(String filenametxt) {
 
-    return 69;
+    return "gay";
   }
 }
